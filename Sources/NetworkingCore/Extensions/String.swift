3eb8477950144e4575f4384aa1281262df0c2abb
Dillon McElhinney <dillon.mce@gmail.com>
2022-06-12 09:23:40 -0600

    Add simple obfuscation

---
+++ Sources/NetworkingCore/Extensions/String.swift
@@ -0,0 +1,15 @@
+//
+//  String.swift
+//  brightwheelExercise
+//
+//  Created by Dillon McElhinney on 6/11/22.
+//
+
+import Foundation
+
+public extension String {
+    func obfuscated() -> String {
+        let bytes = utf8.map { $0 ^ 0b111 }.reversed()
+        return String(bytes: bytes, encoding: .utf8)!
+    }
+}
