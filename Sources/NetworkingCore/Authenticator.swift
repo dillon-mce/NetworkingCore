0f31e3f5765df32cc85594ba8fa03d26718dd98a
Dillon McElhinney <dillon.mce@gmail.com>
2022-06-12 09:23:28 -0600

    Add Authenticator

---
+++ Sources/NetworkingCore/Authenticator.swift
@@ -0,0 +1,43 @@
+//
+//  Authenticator.swift
+//  brightwheelExercise
+//
+//  Created by Dillon McElhinney on 6/11/22.
+//
+
+import AuthenticationServices
+
+/// Wrapper around `ASWebAuthenticationSession`, so everything doesn't have to be an `NSObject` subclass.
+public class Authenticator: NSObject {
+    private let callbackScheme: String
+    private let authenticateURL: URL
+    private var authSession: ASWebAuthenticationSession?
+
+    public init(callbackScheme: String, authenticateURL: URL) {
+        self.callbackScheme = callbackScheme
+        self.authenticateURL = authenticateURL
+    }
+
+    public func authenticate(with parameters: [String: String], handler: @escaping (Result<URL, Error>) -> Void) {
+        let url = authenticateURL.with(parameters: parameters)
+        authSession = ASWebAuthenticationSession(
+            url: url!,
+            callbackURLScheme: callbackScheme,
+            completionHandler: { url, error in
+                if let error = error {
+                    handler(.failure(error))
+                } else if let url = url {
+                    handler(.success(url))
+                }
+            })
+
+        authSession?.presentationContextProvider = self
+        authSession?.start()
+    }
+}
+
+extension Authenticator: ASWebAuthenticationPresentationContextProviding {
+    public func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        ASPresentationAnchor()
+    }
+}
